1aa2c8eebd984a8116b61a7ba2572af879cc222d
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-16 00:20:28 +0200

    fix: screen layout

--- user/wm/hyprland/settings.nix
+++ user/wm/hyprland/settings.nix
@@ -7,16 +7,8 @@ in {
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1, 3440x1440@120, 0x0, 1"
-      "HDMI-A-2,1920x1080@120,3440x100, 1"
-      "HDMI-A-1,3840x2160@120,-300x-2160, 1"
-      #"HDMI-A-1,2560x1440@60,-900x-100, 1.6,transform,3"
-      # "eDP-1,1920x1080@60,0x0, 1"
-      "eDP-1,2560x1600@120,2560x400, 1.6"
-      "DP-3,3840x2160@60,0x0, 1.5"
-      "HEADLESS-2,1920x1080@60,-1920x100, 1"
-      "HEADLESS-3,1280x800@60,1080x1440, 1"
-      "DVI-I-1, 3840x2160@60, 1920x0, 1"
+      "eDP-1,2560x1600@165,2560x400, 1.6"
+      "HDMI-A-1,5120x1440@59.96,2560x0, 1"
       ",preferred,auto,1"
     ];
 
@@ -24,10 +16,10 @@ in {
       "swww-daemon &"
       "ags &"
       # "hyprctl setcursor Catppuccin-Mocha-Lavender-Cursors 24"
-      "[workspace 1 silent] firefox"
-      "[workspace 2 silent] kitty btop"
-      "[workspace 2 silent] kitty ncmpcpp"
-      "[workspace 2 silent] kitty cava"
+      "[workspace 1 silent] google-chrome"
+      "[workspace 2 silent] kitty htop"
+      "[workspace 2 silent] kitty"
+      "[workspace 2 silent] kitty"
     ];
 
     general = lib.mkForce {
