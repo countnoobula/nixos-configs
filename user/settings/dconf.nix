f38feab4da0e96f7ed78d4cc8924569708f570cb
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-16 09:47:23 +0200

    fix: dconf definition

--- user/settings/dconf.nix
+++ user/settings/dconf.nix
@@ -1,12 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  # Enable the dconf module
-  programs.dconf.enable = true;
-
-  # Import your dconf settings from a directory or define them inline
-  # Example: Import from a directory containing key files
-  programs.dconf.settings = {
+  dconf.settings = {
     # virt-manager settings
     "org/virt-manager/virt-manager/connections" = {
       autoconnect = ["qemu:///system"];
