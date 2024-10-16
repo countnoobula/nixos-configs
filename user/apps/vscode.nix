7f9095c471c7976faceade90e0f1b25454c50dc4
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-16 16:38:27 +0200

    vscode extensions hashes

--- user/apps/vscode.nix
+++ user/apps/vscode.nix
@@ -8,65 +8,75 @@
     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       # Helpers
       {
-        publisher = "editorconfig";
         name = "editorconfig";
+        publisher = "editorconfig";
         version = "0.16.4";
+        sha256 = "sha256-dZunfGPgEEppvRHLgr1IzBjgJi1LgYgy5Lkaox/vHhU=";
       }
       {
-        publisher = "github";
         name = "copilot";
+        publisher = "github";
         version = "1.238.0";
+        sha256 = "sha256-6L9Me/1Rr81fHuIzbL08BsrhkUKRxorgNYoZ3kKBkIQ=";
       }
       {
-        publisher = "github";
         name = "copilot-chat";
+        publisher = "github";
         version = "0.21.2";
+        sha256 = "sha256-n66vaKacCZuFOqZsbRdjH9H7JMXo0+IlYgGQiMj1eME=";
       }
 
       # Containers
       {
-        publisher = "hashicorp";
         name = "terraform";
+        publisher = "hashicorp";
         version = "2.33.0";
+        sha256 = "sha256-FXYppKNvHnmXc/lO6jzaW1tp7//d/AJf/SCOZp8jouI=";
       }
       {
-        publisher = "ms-azuretools";
         name = "vscode-docker";
+        publisher = "ms-azuretools";
         version = "1.29.3";
+        sha256 = "sha256-8Ku4z5NtqSv7/fGoAKAlrHaUEyfJGv1YgeUTSZSG5gw=";
       }
+
 
       # CI
       {
-        publisher = "github";
         name = "vscode-github-actions";
+        publisher = "github";
         version = "0.27.0";
+        sha256 = "sha256-2pnFKp97Xjnb94cvQoRbhGIp4cb4t/vG8sa/QayiiHs=";
       }
 
       # Collab
       {
-        publisher = "ms-vsliveshare";
         name = "vsliveshare";
+        publisher = "ms-vsliveshare";
         version = "1.0.5941";
+        sha256 = "sha256-82QGemd//SbeV/ckfk/kdeD+NPK3XbyxfuZHRNdjUlc=";
       }
 
       # Nix
       {
-        publisher = "pinage404";
-        name = "nix-extension-pack";
-        version = "3.0.0";
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.3.5";
+        sha256 = "sha256-uR5rRctC6mpSlOPeQ2MRnJ2xiVYum2Dtxx2/rh2JycM=";
       }
       {
-        publisher = "arrterian";
-        name = "nix-env-selector";
-        version = "1.0.11";
-}
-
-      # PHP
+        name = "nix-extension-pack";
+        publisher = "pinage404";
+        version = "3.0.0";
+        sha256 = "sha256-QBV+NltSO1dshx1BUdoTqJX3O0zPqHJsrQBv/QPoTdg=";
+      }
       {
-        publisher = "bmewburn";
-        name = "vscode-intelephense-client";
-        version = "1.12.6";
-    }];
+        name = "nix-env-selector";
+        publisher = "arrterian";
+        version = "1.0.11";
+        sha256 = "sha256-xmBUdPU1z26qKLUWcqEyOjiZUsFVHePYsgrqOTOSpXQ=";
+      }
+    ];
 
   };
 }
