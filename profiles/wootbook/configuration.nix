25f937f2bdae507e290ff862f052592b67bd4600
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-16 12:22:07 +0200

    fix hyprland import

--- profiles/wootbook/configuration.nix
+++ profiles/wootbook/configuration.nix
@@ -26,7 +26,7 @@
       ../../system/virtualisation/kvm.nix
 
       # Window manager
-      ../../../system/wm/hyprland.nix
+      ../../system/wm/hyprland.nix
     ];
 
   system.stateVersion = "24.05";
