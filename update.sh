840e8f6a3d6111922fbdba8b7ed3d9e7f60466ce
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-16 11:38:00 +0200

    enable update channels

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# Check if a parameter is provided
+if [ -n "$1" ]; then
+  echo "Switching to branch $1"
+  git checkout "$1"
+fi
+
+# Pull the latest changes
 git pull
+
+# Execute the rebuild scripts
 ./rebuild-nixos.sh
 ./rebuild-home.sh
