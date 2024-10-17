5d14f78c398eb2f23b3a774c17c04a0f50a5041a
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2024-10-17 19:07:13 +0200

    update zsh binds for shells

--- user/shell/zsh.nix
+++ user/shell/zsh.nix
@@ -13,11 +13,14 @@
 
     shellAliases = {
       ll = "ls -lah";
-      nix-python = "nix develop ${settings.dotfilesDir}/shells/python -c zsh";
+
       nix-android = "nix develop ${settings.dotfilesDir}/shells/android -c zsh";
+      nix-aws = "nix develop ${settings.dotfilesDir}/shells/aws -c zsh";
+      nix-dotnet = "nix develop ${settings.dotfilesDir}/shells/dotnet -c zsh";
+      nix-java = "nix develop ${settings.dotfilesDir}/shells/java -c zsh";
       nix-nodejs = "nix develop ${settings.dotfilesDir}/shells/nodejs -c zsh";
       nix-php = "nix develop ${settings.dotfilesDir}/shells/php -c zsh";
-      nix-dotnet = "nix develop ${settings.dotfilesDir}/shells/dotnet -c zsh";
+      nix-python = "nix develop ${settings.dotfilesDir}/shells/python -c zsh";
     };
 
     history = {
